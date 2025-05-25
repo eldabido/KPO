9c5d68648453e1f6994a3b8af71f0474bbdac295
David <david_kasparyan1@mail.ru>
2025-05-25 17:57:28 +0300

    Add files via upload

---
+++ BigHW-2/ResultLibrary/AnalysisResult.cs
@@ -0,0 +1,22 @@
+﻿namespace ResultLibrary
+{
+  // Результаты анализа файла.
+  public class AnalysisResult
+  {
+    // Кол-во абзацев.
+    public int ParagraphCount { get; set; }
+
+    // Кол-во слов.
+    public int WordCount { get; set; }
+
+    // Кол-во символов.
+    public int CharacterCount { get; set; }
+
+    // Дата.
+    public DateTime AnalysisDate { get; set; }
+
+    // Вывод.
+    public override string ToString() =>
+        $"Paragraphs: {ParagraphCount}, Words: {WordCount}, Chars: {CharacterCount}";
+  }
+}
---
+++ BigHW-2/TextAnalysisService/Services/TextAnalysisService.cs
@@ -0,0 +1,25 @@
+﻿using ResultLibrary;
+using System.Text.RegularExpressions;
+
+namespace TextAnalysisService.Services;
+
+// Класс, занимающийся анализом текста.
+public class TextAnalysisService : ITextAnalysisService
+{
+  public AnalysisResult AnalyzeText(string text)
+  {
+    return new AnalysisResult
+    {
+      ParagraphCount = CountParagraphs(text),
+      WordCount = CountWords(text),
+      CharacterCount = text.Length,
+      AnalysisDate = DateTime.UtcNow
+    };
+  }
+  // Подсчет абзацев и слов с помощью регулярных выражений.
+  int CountParagraphs(string text) =>
+    text.Split(new[] { "\r\n\r\n", "\n\n", "\r\r" }, StringSplitOptions.RemoveEmptyEntries).Length;
+
+  int CountWords(string text) =>
+    Regex.Matches(text, @"[\p{L}0-9']+").Count;
+}
